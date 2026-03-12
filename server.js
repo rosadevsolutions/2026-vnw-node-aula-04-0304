185ca6f59fe2da0d8903eb0e3d92a8567c264c25
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2026-03-12 16:43:47 -0300

    import express, criacao instancia, definindo port, criacao middleware

---
+++ server.js
@@ -0,0 +1,12 @@
+// 01 - Import do módulo Express para criar o servidor web
+const express = require("express");
+
+// 02 - Criação de uma instância do aplicativo Express
+const app = express();
+
+// 03 - Definição da porta em que o servidor
+// irá escutar (3000 ou a porta definida na variável de ambiente)
+const PORT = process.env.PORT || 3000;
+
+// 04 - Middleware para parsear JSON no corpo das requisições
+app.use(express.json());
